63c80ca46f887c23a06e67ff4ada708a0598a338
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-02-22 11:16:47 -0800

    add open trvia db calls

--- src/utils/questions-api.js
+++ src/utils/questions-api.js
@@ -1,12 +1,25 @@
-const BASE_URL = 'https://opentdb.com/api.php?amount=10';
-const CATEG_URL = 'https://opentdb.com/api_category.php';
-
-export function selectCategory() {
-    return fetch(`${CATEG_URL}`, { mode: "cors" })
-        .then(res => res.json());
+export function getCategories() {
+    const endpoint = 'https://opentdb.com/api_category.php';
+    return fetch(endpoint).then(res => res.json());
 }
 
-export function getQuestions() {
-    return fetch(`${BASE_URL}`, { mode: "cors" })
-        .then(res => res.json());
+export function getTrivia(formData) {
+    const endpoint = 'https://opentdb.com/api.php?';
+    return fetch(`${endpoint}amount=${formData.amount}&category=${formData.category}&difficulty=${formData.difficulty}`).then(res => res.json()).catch((error) => {
+        console.error('Error:', error);
+    });
+};
+
+export function randomizeAnswers(results) {
+    results.forEach(result => {
+        let allAnswers = [...result.incorrect_answers, result.correct_answer]
+        result.allAnswers = allAnswers.sort(() => Math.random() - 0.5)
+    })
+    return results;
+}
+
+export function correctAnswers(results) {
+    return results.reduce((acc, val, idx) => {
+        return { ...acc, [idx]: val.correct_answer }
+    }, {})
 }
