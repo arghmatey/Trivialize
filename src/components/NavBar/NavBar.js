4e423aea70e6d2cd9323d404c627c34169a87ffe
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-09-17 13:41:19 -0700

    style for active nav link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,19 +5,37 @@ import './NavBar.css'
 const NavBar = (props) => {
     let nav = props.user ?
         <div className="Nav">
-            <NavLink to='/skills' className="neon-nav" activeClassName="neon-active">Skills</NavLink>
+            <NavLink 
+                to='/quiz' 
+                className="neon-nav"
+                activeClassName="neon-nav-active">Quizzes</NavLink>
             &nbsp;&nbsp;|&nbsp;&nbsp;
-            <NavLink to='/trivias' className="neon-nav" activeClassName="neon-active">Trivia Games</NavLink>
+            <NavLink 
+                to='/create' 
+                className="neon-nav"
+                activeClassName="neon-nav-active">Create</NavLink>
             &nbsp;&nbsp;|&nbsp;&nbsp;
-            <NavLink to='' onClick={props.handleLogout} className="neon-nav">Logout</NavLink>
+            <NavLink 
+                to='/manage' 
+                className="neon-nav"
+                activeClassName="neon-nav-active">Manage</NavLink>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <NavLink 
+                to='/' 
+                onClick={props.handleLogout} 
+                className="neon-nav">Logout</NavLink>
             &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-            <span className="neon-name">Welcome, {props.user.name}</span>
+            <span className="neon-name">{props.user.name}</span>
         </div>
         :
         <div className="Nav">
-            <NavLink to='/login' className="neon-nav" activeClassName="neon-active">Login</NavLink>
+            <NavLink 
+                to='/login' 
+                className="neon-nav">Login</NavLink>
             &nbsp;&nbsp;|&nbsp;&nbsp;
-            <NavLink to='/signup' className="neon-nav" activeClassName="neon-active">Signup</NavLink>
+            <NavLink 
+                to='/signup' 
+                className="neon-nav">Signup</NavLink>
         </div>;
 
     return (
